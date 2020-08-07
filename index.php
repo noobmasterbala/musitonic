78d5882fae748396df5b309725def48dc9ebd9f5
noobmasterbala <rrbmdu@gmail.com>
2020-08-07 16:33:56 +0530

    locationchanges

--- index.php
+++ index.php
@@ -26,13 +26,13 @@ session_start();
 					<li><a href="Privacy.php">Privacy</a></li>
 					<li><a href="Disclaimer.php">Disclaimer</a></li>
 				</ul>
-				<form action="includes/login.inc.php" method="post">
+				<form action="assets/includes/login.inc.php" method="post">
 					<input type="email" name="mailuid" id="email" placeholder="Email" required><br><br>
 					<input type="password" name="pwd" id="password" placeholder="Password" required><br><br>
 					<button type="submit" name="login-submit">Login</button>
 					<br><a href="assets/php/register.php">Register</a>
 				</form>
-				<form action="includes/logout.inc.php" method="post">
+				<form action="assets/includes/logout.inc.php" method="post">
 					<button type="submit" name="logout-submit">Logout</button>
 				</form>
 				
