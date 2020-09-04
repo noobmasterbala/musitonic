df34868ceda735c79f149eec664027a1436e111a
vijay0523 <vijayvinayak23@gmail.com>
2020-09-04 18:27:07 +0530

    Update submit_new.php

--- assets/includes/submit_new.php
+++ assets/includes/submit_new.php
@@ -3,6 +3,7 @@ if(isset($_POST['submit_password']))
 {
   $email=$_POST['email'];
   $pass=$_POST['confirmpassword'];
+  $pass=password_hash($pass, PASSWORD_DEFAULT);
       require('db_connect.php');
       $query = "UPDATE `users` SET `pwdUsers` = MD5('$pass') WHERE MD5(`emailUsers`) = '$email'";
     $result = mysqli_query($conn, $query);
