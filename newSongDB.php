5456b3f7e8d02b454862846a823e9018c7108054
vijay0523 <vijayvinayak23@gmail.com>
2020-11-01 01:22:41 +0530

    backend for form data to db

---
+++ newSongDB.php
@@ -0,0 +1,45 @@
+<?php
+    require('./assets/includes/db_connect.php');
+    foreach($_REQUEST as $key => $value){
+        echo $key;
+        echo ":";
+        echo $value;
+        echo "<br>";
+        $n=1;
+        $albumname = $_REQUEST['albumname'];
+        $track = $_REQUEST['trackn'.$n];
+        $songfile = $_REQUEST['songfile'.$n];
+        $albumart = $_REQUEST['albumart'.$n];
+        $lang = $_REQUEST['lang'.$n];
+        $date = $_REQUEST['date'.$n];
+        $genre = $_REQUEST['genre'.$n];
+        $subgenre = $_REQUEST['subgenre'.$n];
+        $studio = $_REQUEST['studio'.$n];
+        $vocalist = $_REQUEST['vocalist'.$n];
+        $producer = $_REQUEST['producer'.$n];
+        $artwork = $_REQUEST['artwork'.$n];
+        $mastering = $_REQUEST['mastering'.$n];
+        $mixing = $_REQUEST['mixing'.$n];
+        $composer = $_REQUEST['composer'.$n];
+        $lyricist = $_REQUEST['lycrist'.$n];
+        $crb1 = $_REQUEST['crb1h'.$n].$_REQUEST['crb1m'.$n].$_REQUEST['crb1m'.$n];
+        $crb2 = $_REQUEST['crb2h'.$n].$_REQUEST['crb2m'.$n].$_REQUEST['crb2m'.$n];
+        $crb3 = $_REQUEST['crb3h'.$n].$_REQUEST['crb3m'.$n].$_REQUEST['crb3m'.$n];
+        $query1 = "INSERT INTO `test`( `albumname`, `trackname`, `songname`, `language`, `date`, `genre`, `subgenre`, `studio`, `vocalist`, `producer`, `artworkcredits`, `mastering`, `mixing`, `composer`, `lyricist`, `crb1`, `crb2`, `crb3`, `masteringcheck`, `mixingcheck`, `instrumentalcheck`, `radiocheck`, `explicitcheck`) VALUES ('$albumname','$track','$songfile','$lang','$date','$genre','$subgenre','$studio','$vocalist','$producer','$artwork','$mastering','$mixing','$composer','$lyricist','$crb1','crb2','crb3','masteringcheck','mixingcheck','instrumentalcheck','radiocheck','explicitcheck')";
+        $result = $conn->query($query1) or die($conn->error);
+    }
+    // $query1 = "SELECT COUNT(*) FROM `enrolled` WHERE `online`=1 AND `classid`='$scode'";
+    
+    // $result = $conn->query($query1) or die($conn->error);
+    // while(($row = $result->fetch_row()) !== null){
+    //     $present=$row[0]+0;
+    // }
+    // $query2 = "SELECT COUNT(*) FROM `enrolled` WHERE `classid`='$scode'";
+    // $result = $conn->query($query2) or die($conn->error);
+    // while(($row = $result->fetch_row()) !== null){
+    //     $total=$row[0]+0 - $present;
+    // }
+    // $data = array($present,$total);
+    // echo json_encode($data);
+
+?>
